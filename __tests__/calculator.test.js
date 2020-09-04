e3b845ae3d8a3ac9f2c28f62b9155d953597a7a4
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-04 14:35:23 -0700

    Test 9 - a good fail.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -63,3 +63,11 @@ describe('calculateJupiterExpectancy', () => {
   expect(calculateExpectancy.calculateJupiterExpectancy()).toEqual(78);
   })
 })
+
+describe('mercurySentence', () => {
+
+  test('should return sentence about Mercury using template literals', () => {
+  let userSentence = new Calculator(30, 80);
+  expect(userSentence.mercurySentence()).toEqual("You are 124 years old and based on an 80 year life expectancy, are 44 years past that.");
+  })
+})
