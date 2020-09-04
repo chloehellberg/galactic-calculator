c4f58e21a94cb2cdc3fbd885c01b9e4492b98a2d
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-04 09:50:01 -0700

    First test added and passed with a good fail - the test and calculator.js are connected because I've reached my expectations even though there is currently no method to test on. We know that they are communicating with each other.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,2 +1,9 @@
 import Calculator from './../src/js/calculator';
 
+describe('Calculator', () => {
+
+  test('should correctly calculate a persons age on Mercury', () => {
+    const mercuryAge = new Calculator (30);
+    expect(mercuryAge.calculateMercury).toEqual(124);
+  })
+})
