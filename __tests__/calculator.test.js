3580b47b73185d0a56d38f9ebb4c8df206e62a15
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-04 15:16:07 -0700

    Reorganize of tests complete - will be working on refactoring js so that it uses a library to pull details of each planet so I can make one equation that works for all.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,97 +1,61 @@
 import Calculator from './../src/js/calculator';
 
-describe('calculateMercury', () => {
+describe('Calculate Age For Each Planet', () => {
+  let calculateAge = new Calculator(30);
+  
+test('should correctly calculate a persons current age on Mercury', () => {
+    expect(calculateAge.calculateMercury()).toEqual(124);
+  })
 
-  test('should correctly calculate a persons current age on Mercury', () => {
-    let calculateMercuryAge = new Calculator(30);
-    expect(calculateMercuryAge.calculateMercury()).toEqual(124);
+test('should correctly calculate a persons current age on Venus', () => {
+    expect(calculateAge.calculateVenus()).toEqual(48);
+  })
+
+test('should correctly calculate a persons current age on Mars', () => {
+    expect(calculateAge.calculateMars()).toEqual(15);
+  })
+
+test('should correctly calculate a persons current age on Jupiter', () => {
+    expect(calculateAge.calculateJupiter()).toEqual(2);
   })
 })
 
-describe('calculateVenus', () => {
 
-  test('should correctly calculate a persons current age on Venus', () => {
-    let calculateVenusAge = new Calculator(30);
-    expect(calculateVenusAge.calculateVenus()).toEqual(48);
-  })
-})
-
-describe('calculateMars', () => {
-
-  test('should correctly calculate a persons current age on Mars', () => {
-    let calculateMarsAge = new Calculator(30);
-    expect(calculateMarsAge.calculateMars()).toEqual(15);
-  })
-})
-
-describe('calculateJupiter', () => {
-
-  test('should correctly calculate a persons current age on Jupiter', () => {
-    let calculateJupiterAge = new Calculator(30);
-    expect(calculateJupiterAge.calculateJupiter()).toEqual(2);
-  })
-})
-
-describe('calculateMercuryExpectancy', () => {
-
-  test('should correctly calculate how many years a person has lived past life expectancy on Mercury', () => {
-    let calculateExpectancy = new Calculator(30, 80);
+describe('Calculate Time Left on Each Planet', () => {
+  let calculateExpectancy = new Calculator(30, 80);
+  
+test('should correctly calculate how many years a person has lived past life expectancy on Mercury', () => {
     expect(calculateExpectancy.calculateMercuryExpectancy()).toEqual(-44);
   })
-})
-
-describe('calculateVenusExpectancy', () => {
-
-  test('should correctly calculate how many years a person has left to live on Venus based on life expectancy', () => {
-  let calculateExpectancy = new Calculator(30, 80);
-  expect(calculateExpectancy.calculateVenusExpectancy()).toEqual(32);
+test('should correctly calculate how many years a person has left to live on Venus based on life expectancy', () => {
+    expect(calculateExpectancy.calculateVenusExpectancy()).toEqual(32);
+  })
+test('should correctly calculate how many years a person has left to live on Mars based on life expectancy', () => {
+    expect(calculateExpectancy.calculateMarsExpectancy()).toEqual(65);
+  })
+test('should correctly calculate how many years a person has left to live on Jupiter based on life expectancy', () => {
+    expect(calculateExpectancy.calculateJupiterExpectancy()).toEqual(78);
   })
 })
 
-describe('calculateMarsExpectancy', () => {
 
-  test('should correctly calculate how many years a person has left to live on Mars based on life expectancy', () => {
-  let calculateExpectancy = new Calculator(30, 80);
-  expect(calculateExpectancy.calculateMarsExpectancy()).toEqual(65);
-  })
-})
 
-describe('calculateJupiterExpectancy', () => {
-
-  test('should correctly calculate how many years a person has left to live on Jupiter based on life expectancy', () => {
-  let calculateExpectancy = new Calculator(30, 80);
-  expect(calculateExpectancy.calculateJupiterExpectancy()).toEqual(78);
-  })
-})
-
-describe('mercurySentence', () => {
-
-  test('should return sentence about Mercury using template literals', () => {
+describe('User Sentences', () => {
   let userSentence = new Calculator(30, 80);
-  expect(userSentence.mercurySentence()).toEqual("You are 124 years old and based on an 80 year life expectancy, are -44 years past that.");
+
+test('should return sentence about Mercury using template literals', () => {
+    expect(userSentence.mercurySentence()).toEqual("You are 124 years old and based on an 80 year life expectancy, are -44 years past that.");
   })
-})
 
-describe('venusSentence', () => {
-
-  test('should return sentence about Venus using template literals', () => {
-  let userSentence = new Calculator(30, 80);
-  expect(userSentence.venusSentence()).toEqual("You are 48 years old and based on an 80 year life expectancy, you will live 32 more years.");
+test('should return sentence about Venus using template literals', () => {
+    expect(userSentence.venusSentence()).toEqual("You are 48 years old and based on an 80 year life expectancy, you will live 32 more years.");
   })
-})
 
-describe('marsSentence', () => {
-
-  test('should return sentence about Mars using template literals', () => {
-  let userSentence = new Calculator(30, 80);
-  expect(userSentence.marsSentence()).toEqual("You are 15 years old and based on an 80 year life expectancy, you will live 65 more years.");
+test('should return sentence about Mars using template literals', () => {
+    expect(userSentence.marsSentence()).toEqual("You are 15 years old and based on an 80 year life expectancy, you will live 65 more years.");
   })
-})
-
-describe('Sentence', () => {
-
-  test('should return sentence about Jupiter using template literals', () => {
-  let userSentence = new Calculator(30, 80);
-  expect(userSentence.jupiterSentence()).toEqual("You are 2 years old and based on an 80 year life expectancy, you will live 78 more years.");
+  
+test('should return sentence about Jupiter using template literals', () => {
+    expect(userSentence.jupiterSentence()).toEqual("You are 2 years old and based on an 80 year life expectancy, you will live 78 more years.");
   })
 })
